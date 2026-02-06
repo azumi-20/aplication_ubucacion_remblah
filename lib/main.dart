7e9be21a1b2ad5df18b04857536046f6025252f9
azumi <scarlethcarbajal19@gmail.com>
2026-02-06 12:24:30 -0600

    Cambios de login y registro

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,32 @@
-/*import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login App',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Login App')),
-        body: const Center(child: Text('Hola, Flutter Web!')),
-      ),
-    );
-  }
-} */
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_secure_app/features/auth/presentation/pages/welcome_home.dart';
-//para probar pantallas de inicio de sesión y registro
-import 'package:my_secure_app/features/auth/presentation/pages/welcome_home.dart';
-import 'package:my_secure_app/features/auth/presentation/bloc/auth_bloc.dart';
-import 'package:my_secure_app/features/auth/domain/usecases/register_usecase.dart';
-import 'package:my_secure_app/features/auth/domain/usecases/login_usecase.dart';
-import 'package:my_secure_app/features/auth/domain/repositories/auth_repository.dart';
-import 'package:my_secure_app/features/auth/domain/entities/user_entity.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // generado por flutterfire configure
 
-//repositorio mock (para pruebas)
-class AuthRepositoryMock implements AuthRepository {
-  @override
-  Future<UserEntity> registro(
-    String nombre,
-    String correo,
-    String password,
-  ) async {
-    await Future.delayed(const Duration(seconds: 2));
-    return UserEntity(
-      uid: '123',
-      nombre: nombre,
-      correo: correo,
-      password: password,
-      id: '',
-    );
-  }
-
-  @override
-  Future<UserEntity> login(String correo, String password) async {
-    await Future.delayed(const Duration(seconds: 2));
-    return UserEntity(
-      uid: '123',
-      nombre: 'Usuario Prueba',
-      correo: correo,
-      password: password,
-      id: '',
-    );
-  }
-}
-
-void main() {
-  //inicializamos el repositorio mock o depencias
-  final authRepository = AuthRepositoryMock();
-  final registerUC = RegisterUseCase(authRepository);
-  final loginUC = LoginUseCase(authRepository);
-
-  runApp(
-    //se envuelve MyApp (toda la app) con el BlocProvider
-    BlocProvider(
-      create: (context) => AuthBloc(
-        registerUseCase: registerUC,
-        loginUseCase: loginUC,
-      ),
-      child: const MyApp(),
-    ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Camino de los Sueños',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        primaryColor: const Color(0xFF00C853),
+      title: 'Firebase Auth Test',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Firebase inicializado'),
+        ),
+        body: Center(
+          child: Text(
+            'Firebase está listo para usar 🟢',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
       ),
-      // honme
-      home: const WelcomePage(),
     );
   }
 }
